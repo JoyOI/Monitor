3aed2dea5300ee5928f9759df4d594760a850303
あまみや ゆうこ <1@1234.sh>
2017-12-19 21:22:51 +0800

    Upload infrastructures

---
+++ src/JoyOI.Monitor/Lib/AuthorizeMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace JoyOI.Monitor.Lib
+{
+    public class AuthorizeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthorizeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.Value.ToString().ToLower() != "/account/login" && httpContext.Session.GetInt32("login") != 1)
+            {
+                httpContext.Response.Redirect("/account/login");
+            }
+            else
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AuthorizeMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAuthorize(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthorizeMiddleware>();
+        }
+    }
+}
---
+++ src/JoyOI.Monitor/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using JoyOI.Monitor.Lib;
+
+namespace JoyOI.Monitor
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddConfiguration(out var config);
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddJoyOIUserCenter();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseAuthorize();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
