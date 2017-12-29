ec50c2a2806b40c136eb8c345f4ac3772caa84dc
John Shi <shisoftgenius@gmail.com>
2017-12-30 07:53:32 +0800

    general purpose graph processor

---
+++ src/JoyOI.Monitor/Lib/GraphQueryParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoyOI.Monitor.Lib
+{
+    public class GraphScaling
+    {
+        public int Points { get; set; }
+        public int Interval { get; set; }
+        public GraphScaling(int range, int interval)
+        {
+            this.Points = range / interval; ;
+            this.Interval = interval;
+        }
+    }
+}
