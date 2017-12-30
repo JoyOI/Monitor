ae06dbe8d9fee5c60d9a6b0783c75f642d3aea98
John Shi <shisoftgenius@gmail.com>
2017-12-30 11:40:07 +0800

    refactor

---
+++ src/JoyOI.Monitor/Controllers/StateMachinesChartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using JoyOI.Monitor.Models;
+
+namespace JoyOI.Monitor.Controllers
+{
+    public class StateMachinesChartController : ChartController
+    {
+        const string MGMTSVC = "mgmtsvc";
+
+        [HttpGet]
+        public async Task<IActionResult> Created(int start, int end, int interval)
+        {
+            return Json(await GetChartData(
+                MGMTSVC,
+                "SELECT " +
+                "FLOOR(UNIX_TIMESTAMP(StartTime) / @interval) * @interval as t, " +
+                "Count(Id) as c " +
+                "FROM joyoi_mgmtsvc.statemachineinstances " +
+                "GROUP BY t " +
+                "HAVING t >= @start AND t <= @end" +
+                "ORDER BY t DESC",
+                new ChartScaling(start, end, interval),
+              (rows) => new Chart
+              {
+                  Title = "新建的状态机",
+                  Data = new List<ChartData>() {
+                          new ChartData {
+                              Data = rows.Select(d => Tuple.Create(
+                                  Convert.ToInt64(d["t"].ToString()),
+                                  Convert.ToInt32(d["c"].ToString())
+                              ))
+                              .Where(t => t.Item1 >= start && t.Item1 <= end)
+                              .Select(t => new DataPoint
+                              {
+                                  Timestamp = t.Item1,
+                                  Value = t.Item2
+                              })
+                              .ToList()}
+                        }
+              }
+            ));
+        }
+    }
+}
---
+++ src/JoyOI.Monitor/Models/Chart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoyOI.Monitor.Models
+{
+    public class Chart
+    {
+        public string Title { get; set; }
+        public List<ChartData> Data { get; set; }
+    }
+
+    public class ChartData {
+        public string Title { get; set; }
+        public List<DataPoint> Data { get; set; }
+    }
+
+    public class DataPoint
+    {
+        public long Timestamp { get; set; }
+        public double Value { get; set; }
+    }
+}
