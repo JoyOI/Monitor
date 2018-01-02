b892b34f6597b5ae021293a41c62439ef32c4b0e
John Shi <shisoftgenius@gmail.com>
2018-01-02 23:46:55 +0800

    contest attende chart

--- src/JoyOI.Monitor/Controllers/OnlineJudge/ContestsController.cs
+++ src/JoyOI.Monitor/Controllers/OnlineJudge/ContestsController.cs
@@ -16,7 +16,7 @@ namespace JoyOI.Monitor.Controllers.UserCenter
     public class ContestsController : ChartController
     {
         [HttpGet("Ongoing")]
-        public async Task<IActionResult> Chart(int start, int end, int interval, int timezoneoffset, CancellationToken token)
+        public async Task<IActionResult> Ongoing(int start, int end, int interval, int timezoneoffset, CancellationToken token)
         {
             if (start == 0 || end == 0 || interval == 0)
             {
@@ -67,6 +67,101 @@ namespace JoyOI.Monitor.Controllers.UserCenter
                         }
                     };
                     return new Chart {
+                        Title = title,
+                        Type = "line",
+                        Data = new ChartData
+                        {
+                            Labels = labels.Select(t => ConvertTime(t, timezoneoffset)).ToList(),
+                            Datasets = datasets
+                        },
+                        Options = new
+                        {
+                            Scales = TimeScaleOption()
+                        }
+                    };
+                },
+                token
+            ));
+        }
+
+        [HttpGet("Attende")]
+        public async Task<IActionResult> Attende(int start, int end, int interval, int timezoneoffset, CancellationToken token)
+        {
+            if (start == 0 || end == 0 || interval == 0)
+            {
+                Response.StatusCode = 400;
+                return Json(null);
+            }
+            var scaling = new ChartScaling(start, end, interval);
+            return Json(await GetChartData(
+                JUDGE,
+                @"SELECT 
+                  UNIX_TIMESTAMP(Begin) AS s,
+                  UNIX_TIMESTAMP(ADDTIME(Begin, Duration)) AS e,
+                  CachedAttendeeCount as c,
+                  DisableVirtual as nv
+                  FROM joyoi_oj.contests 
+                  WHERE 
+                  Begin >= DATE_ADD(FROM_UNIXTIME(@start), interval -30 day) 
+                  AND
+                  Begin <  FROM_UNIXTIME(@end)
+                  AND
+                  ADDTIME(Begin, Duration) > FROM_UNIXTIME(@start)",
+                scaling,
+                (rows) => {
+                    string color = "#008b00";
+                    string title = "报名的选手";
+                    var rows_tuple = rows
+                        .Select(d => (
+                            Convert.ToInt64(d["s"]), Convert.ToInt64(d["e"]),
+                            Convert.ToInt64(d["c"]), Convert.ToBoolean(d["nv"])
+                        )).ToList();
+                    var labels = FillMissingAndSort(new List<(long, double)>(), scaling).Select(x => x.Item1);
+                    var virtual_comp = new List<double>();
+                    var nvirtual_comp = new List<double>();
+                    foreach (var l in labels) {
+                        var r_start = l;
+                        var r_end = l + scaling.Interval;
+                        foreach ((long, long, long, bool) row in rows_tuple)
+                        {
+                            int v = 0, nv = 0;
+                            if (row.Item2 >= r_start && row.Item1 <= r_end)
+                            {
+                                if (row.Item4)
+                                {
+                                    // not virtual
+                                    nv++;
+                                }
+                                else
+                                {
+                                    v++;
+                                }
+                            }
+                            virtual_comp.Add(v);
+                            nvirtual_comp.Add(nv);
+                        }
+                    }
+                    var vcolor = RandomColorHex();
+                    var nvcolor = RandomColorHex();
+                    var datasets = new List<ChartDataSet>()
+                    {
+                        new ChartDataSet {
+                            Label = "正式比赛",
+                            Data = nvirtual_comp,
+                            Fill = false,
+                            BackgroundColor = vcolor,
+                            BorderColor = vcolor
+                        },
+                        new ChartDataSet {
+                            Label = "虚拟比赛",
+                            Data = virtual_comp,
+                            Fill = false,
+                            BackgroundColor = nvcolor,
+                            BorderColor = nvcolor
+                        }
+                    };
+                    return new Chart
+                    {
                         Title = title,
                         Type = "line",
                         Data = new ChartData
